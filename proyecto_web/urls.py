bce5e673f4199f626829a1e18134c39c9ead3446
Manuel Ortega <manuel@elarte.coop>
2012-04-10 14:16:00 +0200

    Initial commit

---
+++ proyecto_web/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'proyecto_web.views.home', name='home'),
+    # url(r'^proyecto_web/', include('proyecto_web.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    
+    # App hola mundo /hola/ -> hola()
+    
+    url(r'^hola/$', 'hola_mundo.views.hola'),
+)
